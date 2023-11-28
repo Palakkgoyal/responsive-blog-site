f4515bbe2865873ab90fe6af31329b5a4913668b
Palak <Palakgoyal0304@gmail.com>
2023-11-28 23:34:34 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,38 @@
+const blogs = [
+    {
+        date: "july 23, 2022",
+        title: "Blog one",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        image: "/images/bi1.png"
+    },
+    {
+        date: "july 23, 2022",
+        title: "Blog two",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        image: "/images/bi2.png"
+    },
+    {
+        date: "july 23, 2022",
+        title: "Blog three",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        image: "/images/bi3.png"
+    },
+    {
+        date: "july 23, 2022",
+        title: "Blog four",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        image: "/images/bi4.png"
+    },
+    {
+        date: "july 23, 2022",
+        title: "Blog five",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        image: "/images/bi5.png"
+    },
+    {
+        date: "july 23, 2022",
+        title: "Blog six",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        image: "/images/bi6.png"
+    },
+]
